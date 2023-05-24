190999243bb98aeb16d64810b45dbb504b7aadac
Fernanda <fernandaavellorocha@gmail.com>
2023-05-24 19:06:55 -0400

    Agrego form de contacto

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,3 +16,14 @@ const getMessage = () => {
     message.innerHTML = 'INGRESA LOS DATOS SOLICITADOS ARRIBA'
   }
 }
+
+const getFormData = () => {
+  let formName = document.getElementById('form-name').value
+  let formLastName = document.getElementById ('form-lastname').value
+  let formEmail = document.getElementById('form-email').value
+  let formSel = document.getElementById('form-select')
+  let formSelectText = formSel.options[formSel.selectedIndex].text
+  let formMessage = document.getElementById('form-message').value
+
+  alert(`DE: ${formName} ${formLastName} (${formEmail})\n ASUNTO: ${formSelectText}\n MENSAJE: ${formMessage}`)
+}
